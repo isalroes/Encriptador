58aceca86c25fb17fca884d802d3c473a12ff6b9
isalroes <114121795+isalroes@users.noreply.github.com>
2022-09-25 22:43:30 -0500

    Se agregaron funciones Encriptar y Desencriptar

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
 var llaves = ["ai","enter","imes","ober","ufat"];
+var vocales = ["a","e","i","o","u"];
+var mensaje = "";
 var mensajeEncriptado = "";
+var accion = "";
 
 function ocultarMunieco() {
-    var mensaje = document.getElementById("idTBMensaje").value;
+    mensaje = document.getElementById("idTBMensaje").value;
     if (mensaje == ""){
-        
         document.getElementById("idMensaje01").style.visibility = "visible";
         document.getElementById("idMensaje02").style.visibility = "visible";
         document.getElementById("idMunieco").style.visibility = "hidden";
+        document.getElementById("idTBResultado").style.value = "";
+        document.getElementById("idTBResultado").style.visibility = "hidden";
     }
     else
     {
-         = encriptado(mensaje);
-        document.getElementById("idMunieco").style.visibility = "hidden";
-        document.getElementById("idMensaje01").style.visibility = "hidden";
-        document.getElementById("idMensaje02").style.visibility = "hidden";
-        document.getElementById("idTBResultado").value = mensajeEncriptado;
-        document.getElementById("idTBResultado").style.visibility = "visible"; 
+        if (accion=="Encriptar"){
+            mensajeEncriptado = encriptado(mensaje);
+            document.getElementById("idMunieco").style.visibility = "hidden";
+            document.getElementById("idMensaje01").style.visibility = "hidden";
+            document.getElementById("idMensaje02").style.visibility = "hidden";
+            document.getElementById("idTBResultado").value = mensajeEncriptado;
+            document.getElementById("idTBResultado").style.visibility = "visible";
+            mensaje = ""
+            document.getElementById("idTBMensaje").value = "";
+            accion = "";
+        }
+        else if (accion=="Desencriptar"){
+            mensajeInicial = desencriptado(mensaje);
+            document.getElementById("idMunieco").style.visibility = "hidden";
+            document.getElementById("idMensaje01").style.visibility = "hidden";
+            document.getElementById("idMensaje02").style.visibility = "hidden";
+            document.getElementById("idTBResultado").value = mensajeInicial;
+            document.getElementById("idTBResultado").style.visibility = "visible";
+            mensajeEncriptado = ""
+            document.getElementById("idTBMensaje").value = "";
+            accion = "";
+        }
     }
     
 }
 
 function encriptado(mensajeOriginal){
     var nuevoMensaje = ""
-    largoMensaje = mensajeOriginal.lenght();
-    for (x = 1; x<=largoMensaje; x++){
+    largoMensaje = mensajeOriginal.length;
+    for (x = 0; x<largoMensaje; x++){
         if (mensajeOriginal[x]=="a"){
             nuevoMensaje = nuevoMensaje + llaves[0];
         }
@@ -44,13 +64,50 @@ function encriptado(mensajeOriginal){
             nuevoMensaje = nuevoMensaje + mensajeOriginal[x]
         }
     }    
-    return (nuevoMensaje)
+    return (nuevoMensaje);
+}
+
+function desencriptado(mensajeEncriptado){
+    var mensajeInicial = ""
+    largoMensaje = mensajeEncriptado.length;
+    for (x = 0; x<largoMensaje; x++){
+        if (mensajeEncriptado[x]=="a" && mensajeEncriptado[x+1]=="i"){
+            mensajeInicial = mensajeInicial + vocales[0];
+            x = x+1;
+        }
+        else if (mensajeEncriptado[x]=="e" && mensajeEncriptado[x+1]=="n" && mensajeEncriptado[x+2]=="t" && mensajeEncriptado[x+3]=="e" && mensajeEncriptado[x+4]=="r"){
+            mensajeInicial = mensajeInicial + vocales[1];
+            x = x+4;
+        }
+        else if (mensajeEncriptado[x]=="i" && mensajeEncriptado[x+1]=="m" && mensajeEncriptado[x+2]=="e" && mensajeEncriptado[x+3]=="s"){
+            mensajeInicial = mensajeInicial + vocales[2];
+            x = x+3;
+        }
+        else if (mensajeEncriptado[x]=="o" && mensajeEncriptado[x+1]=="b" && mensajeEncriptado[x+2]=="e" && mensajeEncriptado[x+3]=="r"){
+            mensajeInicial = mensajeInicial + vocales[3];
+            x = x+3;
+        }
+        else if (mensajeEncriptado[x]=="u" && mensajeEncriptado[x+1]=="f" && mensajeEncriptado[x+2]=="a" && mensajeEncriptado[x+3]=="t"){
+            mensajeInicial = mensajeInicial + vocales[4];
+            x = x+3;
+        }
+        else {
+            mensajeInicial = mensajeInicial + mensajeEncriptado[x]
+        }
+    }    
+    return (mensajeInicial);
+    console.log(mensajeOriginal);
+    console.log(nuevoMensaje);
+    console.log(largoMensaje);
+    console.log(x);
 }
 
 function btnEncriptar() {
+    accion = "Encriptar";
     ocultarMunieco();    
 }
 
 function btnDesencriptar () {
-
+    accion = "Desencriptar";
+    ocultarMunieco();    
 }
